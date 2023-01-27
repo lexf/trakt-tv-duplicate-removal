2f8d5ba4997853ac250cb73eb87fad4d3ae598d5
Aleksejs Fadejevs <aleksf87+github@gmail.com>
2023-01-27 22:27:02 +0200

    Add tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @livewireStyles
+        @vite('resources/css/app.css')
         <title>Trakt.tv duplicate removal</title>
     </head>
     <body class="antialiased">
         @livewireScripts
+        <div class="flex items-center justify-center h-screen">
+            <a href="/#" class="hover:bg-blue-400 group flex items-center
+             rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2">
+                Login in with trakt
+            </a>
+        </div>
     </body>
 </html>
